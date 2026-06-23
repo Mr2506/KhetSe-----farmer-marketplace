4a2eb419c5a1b9112576ae1d0717f4cac35730e3
Meetkumar patel <meetkumarp12@gmail.com>
2026-06-24 01:24:43 +0530

    Restore backend folder

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic Test Route
+app.get('/', (req, res) => {
+  res.send('Marketplace API is running...');
+});
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB Connected Successfully'))
+  .catch((err) => console.log('❌ MongoDB Connection Error:', err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
